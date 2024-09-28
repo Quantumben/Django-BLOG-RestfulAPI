84be55e495e9e00296b6107a61a5d8472df1dcb4
QuantumBen <omosabenedict@gmail.com>
2024-09-28 02:27:39 -0700

    Add Token Authentication Middleware

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -134,6 +134,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication',  # Add this line
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # Default permission for views
     ],
 }
