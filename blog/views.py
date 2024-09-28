3e3748225eb6150c7006e8a8adf159237cdc008f
QuantumBen <omosabenedict@gmail.com>
2024-09-28 02:41:00 -0700

    Add Permission for Registration and Login

--- blog/views.py
+++ blog/views.py
@@ -22,8 +22,20 @@ from .serializers import PostSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
 
+from rest_framework.permissions import AllowAny
+from .serializers import RegisterSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+
+
 class RegisterView(APIView):
-    permission_classes = [AllowAny]
+    permission_classes = [AllowAny]  # No authentication required for registration
 
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
@@ -34,6 +46,8 @@ class RegisterView(APIView):
 
 
 class CustomAuthToken(ObtainAuthToken):
+    permission_classes = [AllowAny]  # No authentication required for login
+
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
@@ -84,3 +98,5 @@ class PostDetailView(generics.RetrieveDestroyAPIView):
         if post.user != request.user:
             raise PermissionDenied("You can only delete your own post.")
         return super().delete(request, *args, **kwargs)
+    
+
