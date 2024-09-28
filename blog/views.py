6bacb0d82b9837e4d341d301a2fcefb20d117441
QuantumBen <omosabenedict@gmail.com>
2024-09-28 02:00:15 -0700

    Implement Login functionality

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,10 @@ from .serializers import RegisterSerializer
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
 
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
 class RegisterView(APIView):
     permission_classes = [AllowAny]
 
@@ -14,3 +18,16 @@ class RegisterView(APIView):
             serializer.save()
             return Response({"message": "User created successfully."}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'username': user.username
+        })
