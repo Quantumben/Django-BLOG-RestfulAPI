481611982e2b5815bbf99e1162b34d9feca8f619
QuantumBen <omosabenedict@gmail.com>
2024-09-28 01:52:55 -0700

    Add 'rest_framework' and new app (blog) to INSTALLED_APPS

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'blog',  # The app for our blog API
 ]
 
 MIDDLEWARE = [
