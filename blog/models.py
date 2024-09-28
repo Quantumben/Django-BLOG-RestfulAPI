cca2a0a75aa34357bf12728a8d7f4fabeb8a8789
QuantumBen <omosabenedict@gmail.com>
2024-09-28 02:00:30 -0700

    Creating the Post Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    CATEGORY_CHOICES = [
+        ('TECH', 'Tech'),
+        ('LIFE', 'Life'),
+        ('FOOD', 'Food'),
+        ('TRAVEL', 'Travel'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
